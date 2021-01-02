925206c390c8d8351656a4d9bf07924cfc91997a
TeodorIstan <istan.teodor@gmail.com>
2021-01-02 16:26:55 +0200

    Adaugare comentarii pe o singura linie.

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,10 +1,11 @@
 package ro.java.ctrln;
 
 public class Main {
-    // Aceasta este metoda main, prin care se porneste o aplicatie Java
 
+    // Aceasta este metoda main, prin care se porneste o aplicatie Java
     public static void main(String[] args) {
         // Aici vrem sa afisam pe consola Intellij un mesaj ales de noi.
+        // Aceasta este metoda main, prin care se porneste o aplicatie Java
     System.out.println("Invat Limbajul Java si despre masina virtuala Java!");
     }
 }
